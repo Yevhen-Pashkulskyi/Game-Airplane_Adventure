b3ceafd7c35a558ddcc09e3651ba36911a47bfc6
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-05-19 23:16:22 +0300

    created full move

--- src/main/objects/rocket.py
+++ src/main/objects/rocket.py
@@ -12,25 +12,54 @@ class Rocket(pygame.sprite.Sprite):
         self.direction = "up"
 
     def update(self, keys):
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.direction = "left"
             self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.direction = "right"
             self.rect.x += self.speed
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.direction = "up"
             self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.direction = "down"
             self.rect.y += self.speed
+        if ((keys[pygame.K_UP] or keys[pygame.K_w])
+                and (keys[pygame.K_LEFT] or keys[pygame.K_a])):
+            self.direction = "up-left"
+            self.rect.y -= self.speed/8
+            self.rect.x -= self.speed/8
+        if ((keys[pygame.K_DOWN] or keys[pygame.K_s])
+                and (keys[pygame.K_RIGHT] or keys[pygame.K_d])):
+            self.direction = "down-right"
+            self.rect.y += self.speed/8
+            self.rect.x += self.speed/8
+        if ((keys[pygame.K_DOWN] or keys[pygame.K_s])
+                and (keys[pygame.K_LEFT] or keys[pygame.K_a])):
+            self.direction = "down-left"
+            self.rect.y += self.speed/8
+            self.rect.x -= self.speed/8
+        if ((keys[pygame.K_UP] or keys[pygame.K_w])
+                and (keys[pygame.K_RIGHT] or keys[pygame.K_d])):
+            self.direction = "up-right"
+            self.rect.x += self.speed/8
+            self.rect.y -= self.speed/8
+
 
         if self.direction == "up":
             self.image = pygame.transform.rotate(self.original_img, 0)
-        if self.direction == "right":
-            self.image = pygame.transform.rotate(self.original_img, -90)
+        if self.direction == "up-left":
+            self.image = pygame.transform.rotate(self.original_img, 45)
+        if self.direction == "up-right":
+            self.image = pygame.transform.rotate(self.original_img, -45)
+        if self.direction == "down-left":
+            self.image = pygame.transform.rotate(self.original_img, 135)
+        if self.direction == "down-right":
+            self.image = pygame.transform.rotate(self.original_img, -135)
         if self.direction == "down":
             self.image = pygame.transform.rotate(self.original_img, 180)
+        if self.direction == "right":
+            self.image = pygame.transform.rotate(self.original_img, -90)
         if self.direction == "left":
             self.image = pygame.transform.rotate(self.original_img, 90)
 
